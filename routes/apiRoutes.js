8a68167d320d33f12bf0dcf1da8147e59fd4a652
Ashley Whaley <73979892+AshleyWhaley@users.noreply.github.com>
2021-03-12 23:32:54 -0800

    added api routes and I'm done for the night

---
+++ routes/apiRoutes.js
@@ -0,0 +1,27 @@
+const saved = require('../db/saved');
+const router = require('express').Router();
+
+router.get('/notes', (req,res) => {
+    saved
+    .getNote()
+    .then((notes) => {
+        return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
+});
+
+router.post('/notes', (req, res) => {
+    saved
+    .addNote(req.body)
+    .then((note) => res.json(note))
+    .then((err) => res.status(500).json(err))
+});
+
+router.delete('/notes/:id', (req, res) => {
+    saved 
+    .deleteNote(req.params.ed)
+    .then(() => res.json({ ok: true}))
+    .catch((err) => res.status(500).json(err))
+});
+
+module.exports = router;
